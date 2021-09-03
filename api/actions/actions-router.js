55edbc89d8cfcf6886b77a8edcf2ff00799b1135
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-02 21:14:19 -0400

    added some boiler plate action endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,31 @@
 // Write your "actions" router here!
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    
+})
+
+router.get("/:id", (req, res) => {
+
+})
+
+router.post("/", (req, res) => {
+
+})
+
+router.put("/:id", (req, res) => {
+
+})
+
+router.delete("/:id", (req, res) => {
+
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = router
